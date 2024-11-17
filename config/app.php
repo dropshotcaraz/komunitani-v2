00b109c0e60ae1508869ec7d48e3d7b08d53d41c
dropshotcaraz <ygscavanger@gmail.com>
2024-11-17 13:22:30 +0700

    Update application URL in configuration for production environment

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://komunitani-v2.vercel.app/'),
 
     /*
     |--------------------------------------------------------------------------
