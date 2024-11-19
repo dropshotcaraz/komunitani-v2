91eba0ce773879b22c21934a4dd2dc8f90f802fc
dropshotcaraz <ygscavanger@gmail.com>
2024-11-19 09:56:08 +0700

    refactor: remove unused messages model and update navigation to be sticky

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,4 +1,4 @@
-<nav x-data="{ open: false }" class="bg-[#F6FEDB] border-b border-[#E6D3A3] shadow-md">
+<nav x-data="{ open: false }" class="bg-[#F6FEDB] sticky top-0 border-b border-[#E6D3A3] shadow-md">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto p-2 sm:px-6 lg:px-8">
         <div class="flex justify-between items-center h-16">
