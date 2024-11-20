079e426c5e6288e5cffa9064a85361d2715f59c7
dropshotcaraz <ygscavanger@gmail.com>
2024-11-20 16:03:43 +0700

    feat: add user profile retrieval endpoint with followers and following counts

--- app/Http/Controllers/Api/FollowApiController.php
+++ app/Http/Controllers/Api/FollowApiController.php
@@ -11,6 +11,33 @@ use Illuminate\Support\Facades\Auth;
 class FollowApiController extends Controller
 {
     /**
+     * Get user profile with followers and following
+     *
+     * @param int $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function show($id)
+    {
+        try {
+            $user = User::with(['followers', 'follows'])->findOrFail($id);
+            
+            return response()->json([
+                'success' => true,
+                'data' => [
+                    'user' => $user,
+                    'followers_count' => $user->followers->count(),
+                    'following_count' => $user->follows->count(),
+                ]
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'User not found'
+            ], 404);
+        }
+    }
+
+    /**
      * Follow a user
      *
      * @param Request $request
